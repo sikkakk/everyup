cb7aeedbf4063deac0ce1ac913b7dc2e852d00a7
sikkakk <sikkakki@gmail.com>
2026-09-06 15:37:21 +0900

    Add files via upload

--- everyup-config.js
+++ everyup-config.js
@@ -2,7 +2,7 @@
  * 통합 자동백업 Apps Script를 배포한 뒤 backupApi 한 줄만 배포 URL로 바꾸면 됩니다.
  */
 window.EVERYUP_CONFIG = {
-  backupApi: "https://script.google.com/macros/s/AKfycbxgMAYdU-hIF5EZtAeIvouSujA9cSFTd744MVOFnHf0mFSZa6hWZHcINm1EcEbj5pRlBA/exec",
+  backupApi: "https://script.google.com/macros/s/AKfycbwrBGdH2Dzk28SOqE-CZIL5V05I0jrr9rJW1nU61JF0ZWpeeRU8W1g41M-HFGk-Mpezkw/exec",
   backupToken: "EU26_BACKUP_20260906",
   sessionMs: 60 * 60 * 1000,
   loginUrl: "로그인.html"
